be09e54f0ca0e67eada50c6c1c302a88440c79c5
sayyidsajad <sayyidsajada4s@gmail.com>
2024-02-05 21:34:09 +0530

    started react-meta course with official video

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Counter from "./components/Counter";
+import MealsList from "./components/MealsList";
+import MealsProvider from "./providers/MealsProvider";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MealsProvider>
+        <MealsList />
+        <Counter />
+      </MealsProvider>
     </div>
   );
 }
-
 export default App;
