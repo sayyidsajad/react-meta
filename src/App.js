f3aafa76026fa8c5b53eab06d540dbb4a678d25b
sayyidsajad <sayyidsajada4s@gmail.com>
2024-02-06 19:08:47 +0530

    learning UseContext

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
-import Counter from "./components/Counter";
-import MealsList from "./components/MealsList";
-import MealsProvider from "./providers/MealsProvider";
+import { UserProvider, useUser } from "./UserContext";
 import "./App.css";
 
-function App() {
+const LoggedInUser = () => {
+  const { user } = useUser();
+  return (
+    <p>
+      Hello <span className="Username">{user.name}</span>
+    </p>
+  );
+};
+
+const Header = () => {
+  return (
+    <header>
+      <h2>Blog App</h2>
+      <LoggedInUser />
+    </header>
+  );
+};
+
+const Page = () => {
+  const { user } = useUser();
   return (
     <div>
-      <MealsProvider>
-        <MealsList />
-        <Counter />
-      </MealsProvider>
+      <h2>What is Lorem Ipsum</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi odit
+        aliquam consectetur asperiores ducimus eaque tempora fugiat, quos
+        voluptate quas illum! Laborum natus corporis sequi non est repudiandae
+        totam distinctio!
+      </p>
+      <p>Written by: {user.name}</p>
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <UserProvider>
+      <Header />
+      <Page />
+    </UserProvider>
+  );
+};
 export default App;
---
+++ src/UserContext.js
@@ -0,0 +1,12 @@
+import { createContext, useContext, useState } from "react";
+
+const UserContext = createContext(undefined);
+export const UserProvider = ({ children }) => {
+  const [user] = useState({
+    name: "John",
+    email: "John@example.com",
+    dob: "01/01/2000",
+  });
+  return <UserContext.Provider value={{ user }}></UserContext.Provider>;
+};
+export const useUser = () => useContext(UserContext);
